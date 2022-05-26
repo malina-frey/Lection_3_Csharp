afa96e0c792cd5c55c2ed110de0c14f7e24d66aa
«Alina» <nokian73.1991@mail.ru>
2022-05-26 22:28:54 +0300

    Lection_3_Csharp

---
+++ TASK_1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+вывод таблицы умножения на экран
+*/
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        Console.WriteLine($"{i}x{j}={i * j}");
+    }
+    Console.WriteLine();
+}
---
+++ for/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+for
+*/
+
+string method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = method4(10, "z");
+Console.WriteLine(res);
